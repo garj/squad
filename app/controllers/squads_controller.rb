0915757a24806f82482e3328e93ad0a9aaeb13e8
garjun lam <gar.jun.lam@gmail.com>
2015-06-11 22:41:10 +0000

    first pass load working. ugly code!

--- app/controllers/squads_controller.rb
+++ app/controllers/squads_controller.rb
@@ -27,6 +27,25 @@ class SquadsController < ApplicationController
   
   def show
     @squad = Squad.find(params[:id])
+    
+    @slot1_player = nil
+    @slot2_player = nil
+    @slot3_player = nil
+    @slot4_player = nil
+    
+    
+    if Player.exists?(id: @squad.slot1_player_id)
+     @slot1_player = Player.find(@squad.slot1_player_id)
+    end
+    if Player.exists?(id: @squad.slot2_player_id)
+      @slot2_player = Player.find(@squad.slot2_player_id)
+    end      
+    if Player.exists?(id: @squad.slot3_player_id)
+      @slot3_player = Player.find(@squad.slot3_player_id)
+    end      
+    if Player.exists?(id: @squad.slot4_player_id)
+      @slot4_player = Player.find(@squad.slot4_player_id)     
+    end      
   end
 
 end
